de3a58d4d81ef8a9b6804c4037bd9c0ee57f5e1b
sansone931 <sansone931@users.noreply.github.com>
2023-01-08 13:09:25 +0100

    Update Neovim dap config

--- .config/nvim/lua/keymaps/nvim-dap.lua
+++ .config/nvim/lua/keymaps/nvim-dap.lua
@@ -4,12 +4,6 @@ local M = {}
 function M.setup()
   local wk = require("which-key")
 
-  local function close_debugger()
-    local dap = require("dap")
-    dap.disconnect()
-    dap.close()
-  end
-
   local function set_conditional_breakpoint()
     require("dap").set_breakpoint(
       vim.fn.input('Breakpoint condition: ')
@@ -19,17 +13,21 @@ function M.setup()
   wk.register({
     d = {
       name = "debug",
-      b = { '<Cmd>lua require("dap").toggle_breakpoint()<CR>', "breakpoint"             },
-      B = { set_conditional_breakpoint                       , "conditional breakpoint" },
-      c = { '<Cmd>lua require("dap").run_to_cursor()<CR>'    , "run to cursor"          },
-      d = { '<Cmd>lua require("dap").continue()<CR>'         , "continue"               },
-      h = { '<Cmd>lua require("dap.ui.widgets").hover()<CR>' , "show hover"             },
-      i = { '<Cmd>lua require("dap").step_into()<CR>'        , "step into"              },
-      l = { '<Cmd>lua require("dap").run_last()<CR>'         , "run last"               },
-      o = { '<Cmd>lua require("dap").step_over()<CR>'        , "step over"              },
-      O = { '<Cmd>lua require("dap").step_out()<CR>'         , "step out"               },
-      q = { close_debugger                                   , "stop"                   },
-      w = { '<Cmd>lua require("dapui").toggle()<CR>'         , "toggle debugger ui"     },
+      b = { '<Cmd>lua require("dap").toggle_breakpoint()<CR>'       , "breakpoint"             },
+      B = { set_conditional_breakpoint                              , "conditional breakpoint" },
+      c = { '<Cmd>lua require("dap").run_to_cursor()<CR>'           , "run to cursor"          },
+      d = { '<Cmd>lua require("dap").continue()<CR>'                , "continue"               },
+      D = { '<Cmd>lua require("dap").run_last()<CR>'                , "run last"               },
+      i = { '<Cmd>lua require("dap").step_into()<CR>'               , "step into"              },
+      k = { '<Cmd>lua require("dapui").eval()<CR>'                  , "evaluate"               },
+      o = { '<Cmd>lua require("dap").step_over()<CR>'               , "step over"              },
+      O = { '<Cmd>lua require("dap").step_out()<CR>'                , "step out"               },
+      p = { '<Cmd>lua require("dap").step_back()<CR>'               , "step back"              },
+      P = { '<Cmd>lua require("dap").pause()<CR>'                   , "pause"                  },
+      q = { '<Cmd>lua require("dap").terminate()<CR>'               , "terminate"              },
+      v = { '<Cmd>DapVirtualTextToggle<CR>'                         , "toggle virtual text"    },
+      V = { '<Cmd>DapVirtualTextForceRefresh<CR>'                   , "virtual text refresh"   },
+      w = { '<Cmd>lua require("dapui").toggle({ reset = true })<CR>', "toggle ui"              },
     },
   }, { prefix = "<Leader>" })
 end
--- .config/nvim/lua/plugins/nvim-dap-ui.lua
+++ .config/nvim/lua/plugins/nvim-dap-ui.lua
@@ -2,7 +2,22 @@
 local M = {}
 
 function M.setup()
-  require("dapui").setup()
+  local dap = require("dap")
+  local dapui = require("dapui")
+
+  dapui.setup()
+
+  dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open({ reset = true })
+  end
+  dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+    dap.repl.close()
+  end
+  dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+    dap.repl.close()
+  end
 end
 
 return M
