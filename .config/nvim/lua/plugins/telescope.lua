9119cf24cbf6756343046601e33aeb3fdafabbcb
sansone931 <sansone931@users.noreply.github.com>
2023-03-25 11:54:44 +0100

    Update telescope keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,25 +12,27 @@ return {
     keys = function()
       local builtin = require("telescope.builtin")
 
+      local document_diagnostics = function()
+        builtin.diagnostics({ bufnr = 0 })
+      end
+
       return {
-        { "<Leader>ff", builtin.find_files, desc = "Files" },
-        { "<Leader>fg", builtin.git_status, desc = "Git status" },
-        { "<Leader>fh", builtin.oldfiles, desc = "File history" },
-        { "<Leader>ft", builtin.live_grep, desc = "Text" },
-        { "<Leader>fw", builtin.grep_string, desc = "Word under cursor" },
-        { "<Leader>f?", builtin.builtin, desc = "Builtin pickers" },
         { "<Leader>f/", builtin.current_buffer_fuzzy_find, desc = "Current buffer" },
-        {
-          "<Leader>fd",
-          function()
-            builtin.diagnostics({ bufnr = 0 })
-          end,
-          desc = "Document diagnostics",
-        },
+        { "<Leader>f?", builtin.builtin, desc = "Builtin pickers" },
+        { "<Leader>fb", builtin.buffers, desc = "Buffers" },
+        { "<Leader>fd", document_diagnostics, desc = "Document diagnostics" },
         { "<Leader>fD", builtin.diagnostics, desc = "Workspace diagnostics" },
+        { "<Leader>ff", builtin.find_files, desc = "Files" },
+        { "<Leader>fh", builtin.oldfiles, desc = "Oldfiles" },
+        { "<Leader>fk", builtin.keymaps, desc = "Keymaps" },
+        { "<Leader>fm", builtin.man_pages, desc = "Man pages" },
+        { "<Leader>fn", builtin.notifications, desc = "Notifications" },
+        { "<Leader>fq", builtin.quickfix, desc = "Quickfix" },
+        { "<Leader>fr", builtin.resume, desc = "Resume last search" },
         { "<Leader>fs", builtin.lsp_document_symbols, desc = "Document symbols" },
         { "<Leader>fS", builtin.lsp_dynamic_workspace_symbols, desc = "Workspace symbols" },
-        { "<Leader>fr", builtin.resume, desc = "Resume last search" },
+        { "<Leader>ft", builtin.live_grep, desc = "Text" },
+        { "<Leader>fw", builtin.grep_string, desc = "Word under cursor" },
         { "gd", builtin.lsp_definitions, desc = "Definitions" },
         { "gI", builtin.lsp_implementations, desc = "Implementations" },
         { "gr", builtin.lsp_references, desc = "References" },
