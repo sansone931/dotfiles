d87d6270876df37b9005564f42cbb679ca0f8eab
sansone931 <sansone931@users.noreply.github.com>
2023-07-01 00:46:46 +0200

    Update nvim-dap config
    - Remove external terminal configuration
    - Load dap.configurations from .dap.json

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -52,19 +52,11 @@ return {
 
       vim.fn.sign_define("DapStopped", { linehl = "Visual" })
 
-      dap.defaults.fallback.external_terminal = {
-        command = "/usr/bin/alacritty",
-        args = { "-e" },
-      }
-
       -- {{{ Adapters configuration
-      dap.adapters.node = {
+      dap.adapters.node2 = {
         type = "executable",
         command = "node-debug2-adapter",
       }
-
-      -- Needed for automatic installation with mason-nvim-dap
-      dap.adapters.node2 = dap.adapters.node
 
       dap.adapters.python = {
         type = "executable",
@@ -72,10 +64,15 @@ return {
       }
       -- }}}
 
-      -- Load configuration from .vscode/launch.json
-      require("dap.ext.vscode").load_launchjs(nil, {
-        node = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-      })
+      -- {{{ Load configuration from .dap.json
+      local dap_ext_vscode = require("dap.ext.vscode")
+
+      dap_ext_vscode.type_to_filetypes = {
+        node2 = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+      }
+
+      dap_ext_vscode.load_launchjs(".dap.json")
+      -- }}}
 
       -- Automatically install adapters with mason.nvim
       require("mason-nvim-dap").setup({
