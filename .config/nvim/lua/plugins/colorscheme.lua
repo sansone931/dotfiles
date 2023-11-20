75192016049b0a8052dabada3fc653b7416ebc69
sansone931 <sansone931@users.noreply.github.com>
2023-11-20 11:51:14 -0300

    Update colors in Alacritty, Tmux and Neovim
    Use `night` variant of TokyoNight and remove other colorschemes.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
     priority = 1000,
     config = function()
       require("tokyonight").setup({
+        style = "night",
         on_highlights = function(hl, c)
           -- barbar {{{
           hl.BufferCurrentSign.fg = c.info
@@ -33,24 +34,6 @@ return {
       })
 
       vim.cmd.colorscheme("tokyonight")
-    end,
-  },
-
-  {
-    "gruvbox-community/gruvbox",
-    lazy = true,
-    config = function()
-      vim.g.gruvbox_italic = 1
-      vim.g.gruvbox_invert_selection = 0
-    end,
-  },
-
-  {
-    "shaunsingh/nord.nvim",
-    lazy = true,
-    config = function()
-      vim.g.nord_contrast = true
-      vim.g.nord_borders = true
     end,
   },
 }
