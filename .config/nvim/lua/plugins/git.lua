ad822ee217a6945431f54c1983579f59bf439f34
sansone931 <sansone931@users.noreply.github.com>
2024-05-18 13:00:01 +0200

    Update Neovim git keymaps

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -2,20 +2,19 @@ return {
   {
     "tpope/vim-fugitive",
     event = "VeryLazy",
+    dependencies = { "tpope/vim-rhubarb" },
     keys = {
       { "<Leader>gd", "<Cmd>Gdiffsplit!<CR>", desc = "Diff split" },
       { "<Leader>gB", "<Cmd>Git blame<CR>", desc = "Blame" },
+      { "<Leader>ge", "<Cmd>Gedit<CR>", desc = "Edit" },
+      { "<Leader>gE", ":Gedit ", desc = "Edit (prompt)" },
       { "<Leader>gg", "<Cmd>Git<CR>", desc = "Status" },
       { "<Leader>gh", "<Cmd>0Gclog<CR>", desc = "File commit history" },
       { "<Leader>gl", "<Cmd>Git log<CR>", desc = "Log" },
-      { "<Leader>gx", ":0GBrowse<CR>", mode = { "n", "x" }, desc = "Open in browser" },
-      { "<Leader>gX", ":0GBrowse :%<C-Left>", mode = { "n", "x" }, desc = "Open in browser (prompt)" },
+      { "<Leader>gL", ":Git log ", desc = "Log (prompt)" },
+      { "<Leader>gx", ":0GBrowse!<CR>", mode = { "n", "x" }, desc = "Open in browser" },
+      { "<Leader>gX", ":0GBrowse! :%<C-Left>", mode = { "n", "x" }, desc = "Open in browser (prompt)" },
     },
-  },
-
-  {
-    "tpope/vim-rhubarb",
-    event = "VeryLazy",
   },
 
   {
@@ -29,48 +28,48 @@ return {
         return
       end
 
-      local actions = require("gitsigns.actions")
-
       local prev_hunk = function()
-        actions.nav_hunk("prev")
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
+          gitsigns.nav_hunk("prev")
+        end
       end
 
       local next_hunk = function()
-        actions.nav_hunk("next")
+        if vim.wo.diff then
+          vim.cmd.normal({ "]c", bang = true })
+        else
+          gitsigns.nav_hunk("next")
+        end
+      end
+
+      local blame_line = function()
+        gitsigns.blame_line({ full = true })
+      end
+
+      local stage_hunk_visual = function()
+        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end
+
+      local reset_hunk_visual = function()
+        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
       end
 
       return {
-        { "[g", prev_hunk, mode = { "n", "x" }, desc = "Previous hunk" },
-        { "]g", next_hunk, mode = { "n", "x" }, desc = "Next hunk" },
+        { "[c", prev_hunk, mode = { "n", "x" }, desc = "Previous hunk" },
+        { "]c", next_hunk, mode = { "n", "x" }, desc = "Next hunk" },
+        { "ah", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" }, desc = "Select hunk" },
         { "ih", ":<C-U>Gitsigns select_hunk<CR>", mode = { "o", "x" }, desc = "Select hunk" },
         { "<Leader>gs", gitsigns.stage_hunk, desc = "Stage hunk" },
+        { "<Leader>gS", gitsigns.stage_buffer, desc = "Stage buffer" },
         { "<Leader>gu", gitsigns.undo_stage_hunk, desc = "Undo stage hunk" },
         { "<Leader>gp", gitsigns.preview_hunk, desc = "Preview hunk" },
         { "<Leader>gr", gitsigns.reset_hunk, desc = "Reset hunk" },
         { "<Leader>gR", gitsigns.reset_buffer, desc = "Reset buffer" },
-        {
-          "<Leader>gb",
-          function()
-            gitsigns.blame_line({ full = true })
-          end,
-          desc = "Blame line",
-        },
-        {
-          "<Leader>gs",
-          function()
-            gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end,
-          mode = "x",
-          desc = "Stage hunk",
-        },
-        {
-          "<Leader>gr",
-          function()
-            gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end,
-          mode = "x",
-          desc = "Reset hunk",
-        },
+        { "<Leader>gb", blame_line, desc = "Blame line" },
+        { "<Leader>gs", stage_hunk_visual, mode = "x", desc = "Stage hunk" },
+        { "<Leader>gr", reset_hunk_visual, mode = "x", desc = "Reset hunk" },
       }
     end,
     config = true,
