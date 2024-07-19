f3de5e53a766f600e50c422df87c9f93f976a20e
sansone931 <sansone931@users.noreply.github.com>
2024-07-19 21:57:27 +0200

    Update which-key config

--- .config/nvim/lua/plugins/keymaps.lua
+++ .config/nvim/lua/plugins/keymaps.lua
@@ -6,32 +6,26 @@ return {
       local wk = require("which-key")
 
       wk.setup({
+        delay = 500,
         plugins = {
-          spelling = {
-            enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
-          },
           presets = {
-            operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+            operators = false, -- adds help for operators like d, y, ...
             motions = false, -- adds help for motions
             text_objects = false, -- help for text objects triggered after entering an operator
           },
         },
-        window = {
+        win = {
           border = "single", -- none, single, double, shadow
         },
       })
 
-      wk.register({
-        ["<Leader>b"] = { name = "buffers" },
-        ["<Leader>d"] = { name = "debug" },
-        ["<Leader>f"] = { name = "telescope" },
-        ["<Leader>g"] = { name = "git" },
-        ["<Leader>l"] = { name = "lsp" },
+      wk.add({
+        { "<Leader>b", group = "buffers" },
+        { "<Leader>d", group = "debug" },
+        { "<Leader>f", group = "telescope" },
+        { "<Leader>g", group = "git", mode = { "n", "x" } },
+        { "<Leader>l", group = "lsp" },
       })
-
-      wk.register({
-        ["<Leader>g"] = { name = "git" },
-      }, { mode = "x" })
     end,
   },
 
