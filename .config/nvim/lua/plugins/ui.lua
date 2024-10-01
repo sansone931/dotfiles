2a5faafa110aa49a68c049e66c0aa08bf7c29dd2
sansone931 <sansone931@users.noreply.github.com>
2024-10-01 19:10:26 -0300

    Update lualine config

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -4,7 +4,12 @@ return {
     event = "VeryLazy",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-      extensions = { "fugitive", "nvim-tree" },
+      extensions = { "fugitive", "man", "nvim-dap-ui", "nvim-tree", "quickfix" },
+      options = { section_separators = "", component_separators = "|" },
+      sections = {
+        lualine_b = { "diff", "diagnostics" },
+        lualine_x = { "filetype" },
+      },
     },
   },
 
