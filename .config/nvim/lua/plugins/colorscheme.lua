415707cc1188c5fdf8a0fe655b158aead77cc136
sansone931 <sansone931@users.noreply.github.com>
2024-10-04 09:50:34 -0300

    Remove barbar.nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,31 +6,6 @@ return {
     config = function()
       require("tokyonight").setup({
         style = "night",
-        on_highlights = function(hl, c)
-          -- barbar {{{
-          hl.BufferCurrentSign.fg = c.info
-          hl.BufferCurrent.bg = c.bg_statusline
-          hl.BufferCurrentERROR.bg = c.bg_statusline
-          hl.BufferCurrentHINT.bg = c.bg_statusline
-          hl.BufferCurrentINFO.bg = c.bg_statusline
-          hl.BufferCurrentWARN.bg = c.bg_statusline
-          hl.BufferCurrentIndex.bg = c.bg_statusline
-          hl.BufferCurrentMod.bg = c.bg_statusline
-          hl.BufferCurrentSign.bg = c.bg_statusline
-          hl.BufferCurrentTarget.bg = c.bg_statusline
-
-          hl.BufferVisibleSign.fg = c.bg
-          hl.BufferVisible.bg = c.bg
-          hl.BufferVisibleERROR.bg = c.bg
-          hl.BufferVisibleHINT.bg = c.bg
-          hl.BufferVisibleINFO.bg = c.bg
-          hl.BufferVisibleWARN.bg = c.bg
-          hl.BufferVisibleIndex.bg = c.bg
-          hl.BufferVisibleMod.bg = c.bg
-          hl.BufferVisibleSign.bg = c.bg
-          hl.BufferVisibleTarget.bg = c.bg
-          -- }}}
-        end,
       })
 
       vim.cmd.colorscheme("tokyonight")
