4bc8306fa2eb751864e28035f2ecf2bf2bf5d8bd
sansone931 <sansone931@users.noreply.github.com>
2025-12-01 14:37:14 +0100

    Add missing line to enable jdtls

--- .config/nvim/lua/plugins/lsp/servers/jdtls.lua
+++ .config/nvim/lua/plugins/lsp/servers/jdtls.lua
@@ -2,9 +2,11 @@
 local M = {}
 
 function M.setup()
-  vim.lsp.config('jdtls', {
+  vim.lsp.config("jdtls", {
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
   })
+
+  vim.lsp.enable("jdtls")
 end
 
 return M
